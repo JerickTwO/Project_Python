deb98365a3c1ef2d88242ea712d67cccbcd15b68
Twone <137414207+JerickTwO@users.noreply.github.com>
2023-11-17 18:27:42 -0500

    Commit

--- snake.py
+++ snake.py
@@ -4,18 +4,18 @@ import random
 
 posponer = 0.1
 
-#Marcador 
+#Marcador.
 puntaje = 0
 puntaje_mas_alto = 0
 
-#Fondo
+#Fondo.
 wn = turtle.Screen()
 wn.title('juego de snake')
 wn.bgcolor('black')
 wn.setup(600, 600)
 wn.tracer()
 
-#Texto
+#Texto.
 texto = turtle.Turtle()
 texto.speed(0)
 texto.color("white")
