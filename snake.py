87b818163e620d2ca519254f540621887b82b0ef
Twone <137414207+JerickTwO@users.noreply.github.com>
2023-11-17 05:10:28 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -14,3 +14,13 @@ wn.title('juego de snake')
 wn.bgcolor('black')
 wn.setup(600, 600)
 wn.tracer()
+
+#Texto
+texto = turtle.Turtle()
+texto.speed(0)
+texto.color("white")
+texto.penup()
+texto.hideturtle()
+texto.goto(0,260)
+texto.write("Puntaje      Mayor Puntaje",
+             align = "center", font =("Courier", 20, "normal"))
