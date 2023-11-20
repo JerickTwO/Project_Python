6480e653f68e16564985e1637c98b752100ebafc
Twone <137414207+JerickTwO@users.noreply.github.com>
2023-11-19 19:15:05 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -4,18 +4,18 @@ import random
 
 posponer = 0.1
 
-#Marcador.
+#Marcador 
 puntaje = 0
 puntaje_mas_alto = 0
 
-#Fondo.
+#Fondo
 wn = turtle.Screen()
 wn.title('juego de snake')
 wn.bgcolor('black')
 wn.setup(600, 600)
 wn.tracer()
 
-#Texto.
+#Texto
 texto = turtle.Turtle()
 texto.speed(0)
 texto.color("white")
@@ -24,3 +24,20 @@ texto.hideturtle()
 texto.goto(0,260)
 texto.write("Puntaje      Mayor Puntaje",
              align = "center", font =("Courier", 20, "normal"))
+
+#Cabeza
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape('square')
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = 'stop'
+cabeza.color('green')
+
+#Comida
+comida = turtle.Turtle()
+comida.speed(0)
+comida.shape('circle')
+comida.penup()
+comida.goto(0,100)
+comida.color('red')
